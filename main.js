fed6df0707faea5e59ef3d3cf7b1247bfdb2e2e5
digitalservices5 <161365527+digitalservices5@users.noreply.github.com>
2024-03-25 15:54:55 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env/ node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answers = await inquirer.prompt([
+    { message: (chalk.bgBlue(" enter first number")), type: "number", name: "firstName" },
+    { message: (chalk.bgBlue("enter second number")), type: "number", name: "secondName" },
+    { message: "select one off the operators to perform action",
+        type: "list",
+        name: "operators",
+        choices: ["+", "-", "*", "/"],
+    },
+]);
+if (answers.operators === "+") {
+    console.log(answers.firstName + answers.secondName);
+}
+else if (answers.operators === "-") {
+    console.log(answers.firstName - answers.secondName);
+}
+else if (answers.operators === "*") {
+    console.log(answers.firstName * answers.secondName);
+}
+else if (answers.operators === "/") {
+    console.log(answers.firstName / answers.secondName);
+}
+else {
+    console.log("please select a valid operater");
+}
